bb49998323b9895fd0b5bc480612c3795e1b07d2
Vlad-Leskov <tyabaka@gmail.com>
2024-01-21 21:02:13 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,14 @@
-function getElementWidth(content,padding,border){
-    content = Number.parseFloat(content)
-    padding = Number.parseFloat(padding)
-    border = Number.parseFloat(border)
-    return content+padding*2+border*2
+function checkForSpam(message){
+    const blackListWorld1 = "spam";
+    const blackListWorld2 = "sale";
+    return message.toLowerCase().includes(blackListWorld1.toLowerCase()) || 
+    message.toLowerCase().includes(blackListWorld2.toLowerCase());
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
